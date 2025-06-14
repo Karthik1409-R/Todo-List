9ecbd1101fa37f673f9e2cdb01e4c3f867025ffb
Karthik1409-R <luckykarthik1409@gmail.com>
2025-06-14 20:06:26 +0530

    Updated the html files

---
+++ src/script.js
@@ -0,0 +1,55 @@
+// Menu bar 
+ function changeIcon() {
+        const menubar = document.getElementById('menubar');
+        const icon = menubar.querySelector('i');
+        const navbar = document.getElementById('navbar');
+
+        icon.classList.toggle('fa-bars');
+        icon.classList.toggle('fa-xmark');
+        navbar.classList.toggle('hidden');
+    }
+    document.querySelectorAll('#navbar a').forEach(link => {
+        link.addEventListener('click', () => {
+            const menubar = document.getElementById('menubar');
+            const icon = menubar.querySelector('i');
+            const navbar = document.getElementById('navbar');
+
+            if (!navbar.classList.contains('hidden')) {
+                navbar.classList.add('hidden');
+                icon.classList.remove('fa-xmark');
+                icon.classList.add('fa-bars');
+            }
+        });
+    });
+
+// Redirect button
+document.addEventListener("DOMContentLoaded", function () {
+    const startBtn = document.getElementById("start-btn");
+    if (startBtn) {
+        startBtn.addEventListener("click", () => {
+            window.location.href = "./src/todo.html";
+        });
+    }
+    
+
+    // Init EmailJS
+    emailjs.init("E2tR2S2lZBlXPkzOm");
+
+    // Contact form submit
+    const contactForm = document.getElementById('contact-form');
+    if (contactForm) {
+        contactForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+
+            emailjs.sendForm('service_0ighv7r', 'template_480rfib', this)
+                .then(function (response) {
+                    alert('Email sent successfully!');
+                    console.log('SUCCESS!', response.status, response.text);
+                }, function (error) {
+                    alert('FAILED to send email.');
+                    console.log('FAILED...', error);
+                });
+        });
+    }
+});
+
